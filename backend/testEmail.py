eb42155990b6ffcc0cd1399c78a99a0a2b10c54f
Sonalika <sonalika@igurusolutions.com>
2025-11-17 11:14:39 +0530

    first-commit

---
+++ backend/testEmail.py
@@ -0,0 +1,7 @@
+import fpwd   # adjust this import if your file name is different
+
+# Test the OTP function
+fpwd.otp("your_own_email@gmail.com")
+
+# OR test the sendmail() function
+fpwd.sendmail("your_own_email@gmail.com")
